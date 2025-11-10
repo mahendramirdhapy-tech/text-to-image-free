2ca1ed34dc966022471e5af43149c1c2dbc42413
mahendramirdhapy-tech <mahendramirdhapy@gmail.com>
2025-11-10 14:11:52 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  // Enable CORS
+  // Set CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
@@ -23,15 +23,23 @@ module.exports = async (req, res) => {
       return res.status(400).json({ error: 'Job ID required' });
     }
 
+    console.log(`Checking status for job: ${jobId}`);
+
     // Check Horde job status
-    const statusResponse = await axios.get(`https://aihorde.net/api/v2/generate/check/${jobId}`);
+    const statusResponse = await axios.get(`https://aihorde.net/api/v2/generate/check/${jobId}`, {
+      timeout: 10000
+    });
+    
     const statusData = statusResponse.data;
 
     if (statusData.done) {
-      const resultResponse = await axios.get(`https://aihorde.net/api/v2/generate/status/${jobId}`);
+      const resultResponse = await axios.get(`https://aihorde.net/api/v2/generate/status/${jobId}`, {
+        timeout: 10000
+      });
+      
       const resultData = resultResponse.data;
 
-      if (resultData.generations && resultData.generations.length > 0) {
+      if (resultData.generations && resultData.generations.length > 0 && resultData.generations[0].img) {
         return res.json({
           status: 'completed',
           imageUrl: resultData.generations[0].img
@@ -39,14 +47,15 @@ module.exports = async (req, res) => {
       } else {
         return res.json({
           status: 'failed',
-          error: 'No image generated'
+          error: 'No image generated or image data missing'
         });
       }
     } else {
       return res.json({
         status: 'processing',
         queue_position: statusData.queue_position,
-        wait_time: statusData.wait_time
+        wait_time: statusData.wait_time,
+        done: statusData.done
       });
     }
   } catch (error) {
